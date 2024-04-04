9536f0ddd0acd94c39aaed79ba6d0d66eced80a3
Stephen Greig <steve.greig1@gmail.com>
2024-04-04 01:26:38 +0100

    Missing key fix

--- app/wants-to-work-for/intercom/page.jsx
+++ app/wants-to-work-for/intercom/page.jsx
@@ -305,7 +305,7 @@ const JobApp = () => {
             <p>I've always enjoyed good relationships with my colleagues and managers; Dan, my manager for several years at Adtrak, was kind ennough to provide a very generous reference.</p>
 
             {kindWords.filter(i => i.name === 'Dan Farrow').map(item => (
-              <figure className='flex flex-col w-full p-6 xl:p-8 rounded relative bg-gradient-to-br from-purple to-pink after:absolute after:top-[2px] after:left-[2px] after:right-[2px] after:bottom-[2px] after:bg-navy after:rounded'>
+              <figure key={item.name} className='flex flex-col w-full p-6 xl:p-8 rounded relative bg-gradient-to-br from-purple to-pink after:absolute after:top-[2px] after:left-[2px] after:right-[2px] after:bottom-[2px] after:bg-navy after:rounded'>
                 <blockquote className='relative z-10 border-none testimonial text-gray-400'>
                   &ldquo;{item.quote}&rdquo;
                 </blockquote>
